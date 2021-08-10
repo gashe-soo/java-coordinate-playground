5cc3495f339817349658317403056a56d7d82982
gashe-soo <gashe7@gmail.com>
2021-08-10 21:41:02 +0900

    style:상수화

--- src/main/java/coordinate/domain/Line.java
+++ src/main/java/coordinate/domain/Line.java
@@ -3,6 +3,10 @@ package coordinate.domain;
 import java.util.List;
 
 public class Line extends Shape {
+
+    public static final String IS_NOT_LINE = "직선이 아닙니다.";
+    public static final String LINE_RESULT = "두 점 사이 거리는 ";
+
     public Line(List<Point> points) {
         super(points);
         validateLine(points);
@@ -17,14 +21,14 @@ public class Line extends Shape {
 
     @Override
     public String getInfo() {
-        return "두 점 사이 거리는 " + area();
+        return LINE_RESULT + area();
     }
 
     private void validateLine(List<Point> points) {
         Point firstPoint = points.get(0);
         Point secondPoint = points.get(1);
         if (firstPoint.equals(secondPoint)) {
-            throw new IllegalArgumentException("직선이 아닙니다.");
+            throw new IllegalArgumentException(IS_NOT_LINE);
         }
     }
 }
